718d946383dd2429670fba2ec5396e8b0ee92160
Pablo Roca <Pablo Roca@LAPTOP-VLKALNAC>
2018-02-25 20:41:09 -0600

    25/02/2018

--- src/ipc1/practica1_201700584/IPC1Practica1_201700584.java
+++ src/ipc1/practica1_201700584/IPC1Practica1_201700584.java
@@ -1,12 +1,46 @@
 
 package ipc1.practica1_201700584;
+import java.util.Scanner;
 
 public class IPC1Practica1_201700584 {
 
     
     public static void main(String[] args) {
         
-        
+       Scanner entrada = new Scanner(System.in);
+       int OpMenuPrincipal = 0;
+       
+       do{
+           //Menu Principal del juego
+           System.out.println("1. Dificultad del juego");
+           System.out.println("2. Parametros del juego");
+           System.out.println("3. Iniciar juego");
+           System.out.println("4. Salir");
+           System.out.println();
+           System.out.print("Escoja un opcion: ");
+           OpMenuPrincipal = entrada.nextInt();
+           System.out.println();
+           
+           switch(OpMenuPrincipal){
+               
+               case 1:
+                   break;
+                
+               case 2:
+                   break;
+                   
+               case 3:
+                   break;
+                   
+               case 4:
+                   break;
+                   
+               default:
+                   break;
+               
+           }
+           
+       }while(OpMenuPrincipal != 4);
 
     }
     
